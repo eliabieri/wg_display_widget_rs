52e899c01e9f661af65eb0dd1384e2f1ed093cb8
Elia Bieri <contact@eliabieri.com>
2023-05-08 17:24:07 +0200

    Add initial widget example

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# wg_display_widget_rs
-Template repository for creating a wg_display widget in Rust
+# WG Display widget template for Rust
+
+Template repository for creating a [WG Display](https://github.com/eliabieri/wg_display) widget in Rust.  
+The example widget demonstrates how to make an HTTP request, parsing the response and returning the result.  
+The complete API that is provided by the host application can be seen unser `wg_display_widget_wit/wit`.
+
---
+++ widget/src/lib.rs
@@ -0,0 +1,78 @@
+use serde_json::Error;
+use serde_json::Value;
+
+use schemars::{schema_for, JsonSchema};
+use serde::Deserialize;
+
+wit_bindgen::generate!({
+    path: "../wg_display_widget_wit/wit",
+    world: "widget"
+});
+
+#[derive(JsonSchema, Deserialize)]
+struct WidgetConfig {
+    city: String,
+}
+
+const WIDGET_NAME: &str = "Aare Temperature";
+
+struct MyWidget;
+
+impl Widget for MyWidget {
+    fn get_name() -> wit_bindgen::rt::string::String {
+        WIDGET_NAME.into()
+    }
+
+    fn run(config: WidgetContext) -> WidgetResult {
+        logging::log(logging::Level::Info, WIDGET_NAME, "Widget run started");
+
+        if "{}" == config.config {
+            return WidgetResult {
+                data: "No config provided".into(),
+            };
+        }
+
+        let config: WidgetConfig =
+            serde_json::from_str(&config.config).expect("Failed to parse config");
+
+        let response = http::request(
+            http::Method::Get,
+            format!(
+                "https://aareguru.existenz.ch/v2018/today?city={}",
+                config.city
+            )
+            .as_str(),
+            None,
+        );
+        let Ok(response) = response else {
+            return WidgetResult {
+                data: "Failed to make network request".into(),
+            };
+        };
+
+        if 200 != response.status {
+            return WidgetResult {
+                data: format!("Response status != 200: {}", response.status),
+            };
+        }
+
+        let data: Result<Value, Error> = serde_json::from_slice(response.bytes.as_slice());
+        let result = match data {
+            Ok(data) => format!("{}: {} C", config.city, data["aare"]),
+            Err(_) => "Response from AareGuru could not be parsed".into(),
+        };
+
+        WidgetResult { data: result }
+    }
+
+    fn get_config_schema() -> wit_bindgen::rt::string::String {
+        let schema = schema_for!(WidgetConfig);
+        serde_json::to_string_pretty(&schema).unwrap()
+    }
+
+    fn get_version() -> wit_bindgen::rt::string::String {
+        "1.0.0".into()
+    }
+}
+
+export_widget!(MyWidget);
